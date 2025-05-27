9d656c501e0351640bd69c0ccef737f195bb5575
starle21 <staron.lenka@gmail.com>
2025-05-27 20:43:44 +0200

    F2: defining type Todo, loading with fetch in useEffect. This approach of using useEffect to load data is not recommended, but for such a small application, it is acceptable. The general approach I follow is to use as little 3rd party libraries as possible to cut down on coupling.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,50 @@ import { List } from "./components/List";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { ThemeProvider } from "./components/providers/ThemeProvider";
+import { useEffect, useState } from "react";
+import { ListItem } from "./components/ListItem";
 
-export const App = () => (
-    <ThemeProvider>
-        <Container>
-            <Layout>
-                <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
-                <List />
-                <Footer />
-            </Layout>
-        </Container>
-    </ThemeProvider>
-);
+type Todo = {
+    id: number;
+    label: string;
+    isDone: boolean;
+    createdAt: number;
+};
+
+export const App = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            const request = await fetch("http://localhost:3000/items");
+            const todos = await request.json();
+            setTodos(todos);
+        };
+        makeRequest();
+    }, []);
+
+    return (
+        <ThemeProvider>
+            <Container>
+                <Layout>
+                    <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
+                    <List>
+                        {todos.map(({ id, label, isDone }) => {
+                            return (
+                                <ListItem
+                                    key={id}
+                                    label={label}
+                                    isDone={isDone}
+                                    onItemDelete={() => {}}
+                                    onItemDoneToggle={() => {}}
+                                    onItemLabelEdit={() => {}}
+                                />
+                            );
+                        })}
+                    </List>
+                    <Footer />
+                </Layout>
+            </Container>
+        </ThemeProvider>
+    );
+};
